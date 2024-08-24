0fd31c9dde6f34444d00c048bd44d346498630de
Unviribde <153086918+Klysheee@users.noreply.github.com>
2024-08-24 16:19:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import os
 from discord.ext import commands
 from discord import Intents
 
-# Configuration
 openweather_api_key = 'your_openweather_api_key_here'
 bot_token = 'your_discord_bot_token_here'
 
@@ -13,7 +12,6 @@ intents.guilds = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Load cogs
 async def setup_cogs():
     for filename in os.listdir('./cogs'):
         if filename.endswith('.py') and filename != '__init__.py':
